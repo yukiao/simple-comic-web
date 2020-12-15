45b65926382701615e81064e305725f086060677
yukiao <lembang19sony@gmail.com>
2020-12-15 21:30:58 +0800

    Reset info and about page

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -7,11 +7,6 @@
         <div class="row">
             <div class="col">
                 
-                <h1>Hello I'am Supri</h1>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo soluta eos, fugit sint quas quisquam doloribus dicta sapiente exercitationem. Veniam error id reiciendis assumenda vel dignissimos odio nulla consectetur officiis.</p>
-            
-               
-
             </div>
         </div>
     </div>
--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -6,14 +6,6 @@
 <div class="container">
         <div class="row">
             <div class="col">
-                
-               <?php foreach ($alamat as $a) : ?>
-                <ul>
-                    <li><?=$a['tipe']; ?></li>
-                    <li><?=$a['alamat']; ?></li>
-                    <li><?=$a['kota']; ?></li>
-                </ul>
-<?php endforeach; ?>
             </div>
         </div>
     </div>
